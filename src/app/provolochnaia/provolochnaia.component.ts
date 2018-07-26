f06cdbd2f7a62b9af09136bbbd1fb13a4702995b
Bogdan <bogdanshcherbak56@gmail.com>
2018-07-27 00:36:19 +0300

    Реактвный калькулятор. Использовал Pipe для этого. Пофиксил сразу все кейсы , чтобы не было багов

--- src/app/provolochnaia/provolochnaia.component.ts
+++ src/app/provolochnaia/provolochnaia.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, OnChanges, SimpleChanges } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { CalcViewModel } from './calc.model';
 @Component({
   selector: 'app-provolochnaia',
   templateUrl: './provolochnaia.component.html',
   styleUrls: ['./provolochnaia.component.css']
 })
 export class ProvolochnaiaComponent implements OnInit {
+  ngOnInit(): void {
+    this.model = new CalcViewModel();
+  }
+  // tslint:disable-next-line:member-ordering
+  options: FormGroup;
 
-  constructor() { }
+  // tslint:disable-next-line:member-ordering
+  model: CalcViewModel;
 
-  ngOnInit() {
+
+  onChangeRepeat = (val: any) => {
+    this.model.inputFieldValue = val === '' ? 1 : val;
   }
 
 }
